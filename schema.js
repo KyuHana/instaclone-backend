a095ddc1045416d6349906b1ee95562df2df471d
KyuHana <46952107+KyuHana@users.noreply.github.com>
2021-06-09 22:58:25 +0900

    왜 안될까

--- schema.js
+++ schema.js
@@ -1,16 +1,9 @@
-import { loadFilesSync, mergeResolvers, mergeTypeDefs, makeExecutableSchema } from 'graphql-tools';
-console.log('1'); 
-console.log(`${__dirname}`); 
+import { loadFilesSync, mergeResolvers, mergeTypeDefs, makeExecutableSchema } from "graphql-tools";
+
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedTypess = loadFilesSync(`${__dirname}/**/*.queries.js`);
-console.log(loadedTypess);
-console.log(loadedTypes);
 const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries, mutations}.js`);
-console.log(loadedResolvers);
 const typeDefs = mergeTypeDefs(loadedTypes);
-const resolvers = mergeResolvers(loadedResolvers);
-console.log(typeDefs);
-console.log(resolvers);
+const resolvers = mergeResolvers(loadedResolvers); 
 const schema = makeExecutableSchema(typeDefs, resolvers);
 
 export default schema;
